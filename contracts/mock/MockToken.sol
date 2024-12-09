b92be99e92b3e4af9b08150ff8bc14e963a5e191
kavsky02 <kamilovsky@deltaprime.io>
2024-12-09 16:58:31 +0100

    Add MockTokens deployment files

--- contracts/mock/MockToken.sol
+++ contracts/mock/MockToken.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
+    uint256 public constant MAX_SUPPLY = 100 * 10 ** 18; // 100 tokens with 18 decimals
+
     constructor() ERC20("MCKTKN18", "MockToken18") {}
 
     function decimals() public pure override returns (uint8) {
@@ -11,6 +13,8 @@ contract MockToken is ERC20 {
     }
 
     function mint() external {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+        require(balanceOf(msg.sender) == 0, "Already have tokens");
+        require(totalSupply() + (10 * 10 ** decimals()) <= MAX_SUPPLY, "Max supply reached");
+        _mint(msg.sender, 10 * 10 ** decimals());
     }
 }
